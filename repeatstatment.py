bde8adc81a86d074714b6b46985ece66a71898ad
Ashwani <aswanibtech@gmail.com>
2019-12-16 15:46:34 +0530

    loop

---
+++ repeatstatment.py
@@ -0,0 +1,37 @@
+print("Happy B-day",end="")
+print("Happy B-day")
+'''
+intilization
+while condition:
+   task to be repeated
+   updation
+
+
+i = 1
+while i<=10:
+    print(i,"Happy B-day")
+    #print(i,end="")
+    i=i+1
+    
+
+
+i=1
+while i<10:
+    if i%2==0:
+        #print("Even no=",i)
+        pass
+    else:
+        print("Odd no=",i)
+    i=i+1 
+'''
+
+#break continue and pass
+i=1
+while i<=10:
+    if i%3==0:
+        #break
+        #pass
+        continue
+    else:
+        print(i)
+    i=i+1
